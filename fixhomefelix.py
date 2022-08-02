f70e7250625f54be0134b41641740736a3decd2a
rperez23 <ronnie.lperez@gmail.com>
2022-08-02 11:21:32 -0700

    004

--- fixhomefelix.py
+++ fixhomefelix.py
@@ -3,11 +3,16 @@
 import os
 import re
 import sys
+import time
 import argparse
 import openpyxl
 import warnings
 import subprocess
 from openpyxl.utils.cell import get_column_letter
+
+tab = "1. Master Metadata"
+
+warnings.simplefilter(action='ignore', category=UserWarning)
 
 def getXLF():
 
@@ -16,9 +21,52 @@ def getXLF():
         s3path = input("  Give me your AWS S3 Path: ")
         lscmd  = "aws s3 ls \"" + s3path + "\" | grep xlsx | awk '{print $4}'"
         xlf = os.popen(lscmd).read()
+        time.sleep(5)
+
+        xlf = xlf.strip()
         m = re.search('\.xlsx$',xlf)
         if m:
             break
+
+    cpcmd = "aws s3 cp \"" + s3path + xlf + "\" ."
+    print(" ",cpcmd)
+    cpoutput = os.popen(cpcmd)
+    time.sleep(5)
+
+    if not os.path.exists(xlf):
+        print("  ~~Could not copy xlf file")
+        sys.exit(1)
     print("")
 
-getXLF()
+
+def openXLF(xlf):
+
+    try:
+        workbook = openpyxl.load_workbook(filename=xlf)
+    except:
+        print("  Cannot open",xlf)
+        sys.exit(0)
+
+    #validate that sheet exists
+    if not (tab in workbook.sheetnames):
+        print(" ",tab,"not in",xlf)
+        sys.exit(0)
+
+    #assign the work sheet object
+    ws = workbook[tab]
+
+    return workbook, ws
+
+
+#1) Get the XLF File
+xlf = getXLF()
+
+
+"""
+#2) open the XLF File: get workbook and worksheet
+workbook,ws = openXLF(xlf)
+
+#?) close the workbook
+#workbook.save(xlf)
+workbook.close()
+"""
