a304f352bc799517a8a2fe39c860ff26082bb692
rperez23 <ronnie.lperez@gmail.com>
2022-08-02 11:21:23 -0700

    003

--- fixhomefelix.py
+++ fixhomefelix.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import os
+import re
 import sys
 import argparse
 import openpyxl
@@ -11,11 +12,13 @@ from openpyxl.utils.cell import get_column_letter
 def getXLF():
 
     print("")
-    s3path = input("  Give me your AWS S3 Path: ")
-    lscmd  = "aws s3 ls \"" + s3path + "\" | grep xlsx | awk '{print $4}'"
-    #print(lscmd)
-    xlf = os.popen(lscmd).read()
-    print(xlf)
+    while True:
+        s3path = input("  Give me your AWS S3 Path: ")
+        lscmd  = "aws s3 ls \"" + s3path + "\" | grep xlsx | awk '{print $4}'"
+        xlf = os.popen(lscmd).read()
+        m = re.search('\.xlsx$',xlf)
+        if m:
+            break
     print("")
 
 getXLF()
