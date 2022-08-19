353c387fc9317ecc1ad354d5bd5f74214918d8d0
rperez23 <ronnie.lperez@gmail.com>
2022-08-19 03:58:07 -0700

    v002

--- priceiswrong.pl
+++ priceiswrong.pl
@@ -1,8 +1,15 @@
 #! /usr/bin/perl
 
-$inf = "infile.txt";
+$inf  = "infile.txt";
+$outf = "log.txt";
 
 @GlacierList = ();
+
+unless (open OUTF, ">$outf")
+{
+  print("Cannot open output file $outf\n");
+  exit(1);
+}
 
 sub getMOV
 {
@@ -29,6 +36,33 @@ sub getMOV
 }
 
 
+sub getMP3
+{
+  $f = $_[0];
+  $cmd = "./convertmov.py $f";
+
+  print("  ~~$cmd~~\n");
+
+  unless (open CMD, "$cmd |")
+  {
+    print("Cannot run command $cmd\n");
+    exit(1);
+  }
+
+  while(<CMD>)
+  {
+    chomp;
+    print("$_\n");
+  }
+  close(CMD);
+
+  $exitstat = $? >> 8;
+  return $exitstat;
+
+}
+
+
+
 unless (open INF, "<$inf")
 {
   print("  ~~Cannot open input file $inf\n");
@@ -44,15 +78,33 @@ while(<INF>)
 
   if ($stat == 0)
   {
-    #copy successful convert it
-    
+
+    @parts = split/\//,$mov;
+    $movf  = pop(@parts);
+    $movf  =~ s/"//;
+    #print("$movf\n");
+
+    $exstat = &getMP3($movf);
+
+    if ($exstat == 0)
+    {
+      system("rm $mov");
+      print OUTF ("$mov : CONVERTED\n");
+    }
+
+
   }
   elsif($stat == 2)
   {
     push(@GlacierList,$mov);
-    #print(@GlacierList);
+    print OUTF ("$mov : GLACIER\n");
+  }
+  else
+  {
+    print OUTF ("$mov : ERROR\n");
   }
   print("\n==========\n");
 
 }
 close(INF);
+close(OUTF);
